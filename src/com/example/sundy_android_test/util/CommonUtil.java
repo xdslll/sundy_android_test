a23859050eaf36b50fa7fee5fc23a2e77110db02
sam <xdslll@163.com>
2014-06-11 10:47:57 +0800

    commit chapter1_1 source

---
+++ src/com/example/sundy_android_test/util/CommonUtil.java
@@ -0,0 +1,14 @@
+package com.example.sundy_android_test.util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by xiads on 14-6-11.
+ */
+public class CommonUtil {
+
+    public static void ShowToastMessage(Context c, String msg) {
+        Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ src/com/example/sundy_android_test/util/Constant.java
@@ -0,0 +1,9 @@
+package com.example.sundy_android_test.util;
+
+/**
+ * Created by xiads on 14-6-11.
+ */
+public interface Constant {
+
+    public static final String DEBUG_TAG = "TAG";
+}
